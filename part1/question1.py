4aff67c11aa7c70cf5e4f4cdbf0ff0439aafacd4
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2023-10-30 10:05:06 -0500

    Solve question 1.1

--- part1/question1.py
+++ part1/question1.py
@@ -20,16 +20,21 @@ def get_city_temperature(city):
       return 17
    if city == "San Francisco":
       return 16
+   if city == "New York":
+      return 14
 
 def get_city_weather(city):
 
-  sky_condition = None
+   sky_condition = None
 
-  if city == "Sao Paulo":
-     sky_condition = "cloudy"
-  elif city == "New York":
-     sky_condition = "rainy"
+   if city == "Sao Paulo":
+      sky_condition = "cloudy"
+   elif city == "New York":
+      sky_condition = "rainy"
+   elif city == "Quito":
+      sky_condition = "sunny"
+   elif city == "San Francisco":
+      sky_condition = "partly cloudy"
 
-  temperature = get_city_temperature(city)
-
-  return str(temperature) + " degrees and " + sky_condition
+   temperature = get_city_temperature(city)
+   return str(temperature) + " degrees and " + sky_condition
